f8da9f305b42739afc6502378162d754db8cfbc3
Leandro Cocchi <leandro.cocchi@comafi.com.ar>
2020-05-17 10:04:36 -0300

    Introduccion a POO con un pequeno ejemplo

---
+++ Tutorial POO/src/tutorial/poo/Documento.java
@@ -0,0 +1,17 @@
+package tutorial.poo;
+
+public class Documento {
+
+    int numero;
+    String nombre;
+    String direccion;
+    
+  
+    Documento(int numero, String nombre, String direccion) {
+        this.numero = numero;
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+    
+
+}
